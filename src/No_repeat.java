0fb8a583b111e9e8fd93a384e5f5969bf7141d68
Manu Agrawal <agrawalmanu73@gmail.com>
2025-04-23 12:48:46 +0530

    New

---
+++ src/No_repeat.java
@@ -0,0 +1,16 @@
+public class No_repeat {
+    public static void main(String[] args) {
+        String str= "hhlloo";
+        String newstr= "";
+        for (int i=0 ;i<str.length();i++)
+        {
+            char c=str.charAt(i);
+            if (newstr.indexOf(c)==-1)
+            {
+                newstr=newstr+c;
+            }
+        }
+        System.out.println(newstr + "  ");
+
+    }
+}
