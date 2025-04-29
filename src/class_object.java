6fe4f306b090f426b3800b467d9a3a5696baa833
Manu Agrawal <agrawalmanu73@gmail.com>
2025-04-29 13:02:38 +0530

    class and object

---
+++ src/class_object.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Student{
+    String name="modi";
+    void sum(int a,int b){
+        System.out.println(a+b);
+    }
+}
+
+public class class_object {
+    public static void main(String[] args) {
+        Student obj=new Student();
+        Student obj=new Student();
+        obj.sum(5,2);
+    }
+}
