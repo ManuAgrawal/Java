a8fe3a4f65590d5ace19e62cf8997fb7e47c40d6
Manu Agrawal <agrawalmanu73@gmail.com>
2025-06-14 16:52:28 +0530

    new

---
+++ src/setmap.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class setmap {
+    public static void main(String[]args){
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("id0" , 1)  ;
+        map.put("id1" , 2)  ;
+        map.put("id2" , 3)  ;
+        map.put("id3" , 4)  ;
+        System.out.println(map.get("id0"));
+        System.out.println(map.get("id3"));
+        System.out.println(map.containsKey("id2"));
+
+        map.remove("id2");
+        System.out.println(map.get("id2"));
+        System.out.println(map.containsKey("id2"));
+
+        for (String key : map.keySet()) {
+            System.out.println(key);
+
+        }
+        for (Integer value : map.values()) {
+            System.out.println(value);
+        }
+        map.forEach((key, value) -> {
+            // if (value.equals()){
+            //     System.out.println(key);
+
+            System.out.println( value);
+            //}
+
+
+        });
+
+
+    }
+}
